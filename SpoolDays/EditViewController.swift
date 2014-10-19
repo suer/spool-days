9bb95e0c8234dcad54ebeb5013c25bc489f53e50
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-10-19 20:04:44 +0900

    Added: date field

--- SpoolDays/EditViewController.swift
+++ SpoolDays/EditViewController.swift
@@ -1,9 +1,13 @@
 import UIKit
 
-class EditViewController: UIViewController {
+class EditViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var textView: UITextView?
     let dateViewModel: DateViewModel
+    var tableView: UITableView?
+
+    let textViewHeight = CGFloat(150.0)
+    let cellHeight = CGFloat(50.0)
 
     convenience init (dateViewModel: DateViewModel) {
         self.init(nibName: nil, bundle: nil, dateViewModel: dateViewModel)
@@ -25,6 +29,7 @@ class EditViewController: UIViewController {
         loadCancelButton()
         loadSaveButton()
         loadTextView()
+        loadTableView()
     }
 
     func loadCancelButton() {
@@ -46,9 +51,41 @@ class EditViewController: UIViewController {
     }
 
     func loadTextView() {
-        let textView = UITextView(frame: CGRectMake(0, 0, view.bounds.width, 200))
-        textView.text = dateViewModel.title
-        view.addSubview(textView)
+        textView = UITextView(frame: CGRectMake(0, 0, view.bounds.width, textViewHeight))
+        textView!.text = dateViewModel.title
+        textView!.layer.borderWidth = 1
+        textView!.layer.borderColor = UIColor(white: 0.5, alpha: 0.5).CGColor
+        view.addSubview(textView!)
+    }
+
+    func loadTableView() {
+        tableView = UITableView(frame: CGRectMake(0, textViewHeight, view.bounds.width, cellHeight))
+        tableView!.delegate = self
+        tableView!.dataSource = self
+        view.addSubview(tableView!)
+    }
+
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.0
+    }
+
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return cellHeight
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int  {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Date"
+        let date = NSDate()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd"
+        cell.detailTextLabel?.text = dateFormatter.stringFromDate(date)
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
