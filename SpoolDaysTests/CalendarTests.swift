59e0e990b8848b632a1e6c5a44161a124d7c41fd
Ryo SUETSUGU <suetsugu.r@gmail.com>
2015-02-24 00:01:43 +0900

    Added: test

--- SpoolDaysTests/CalendarTests.swift
+++ SpoolDaysTests/CalendarTests.swift
@@ -1,11 +1,20 @@
 import XCTest
 
 class CalendarTests: XCTestCase {
+    func testDateIntervalFromDate() {
+        let cal = NSCalendar(identifier: NSGregorianCalendar)!
+        let date = cal.dateWithEra(1, year: 1981, month: 8, day: 4, hour: 20, minute: 0, second: 0, nanosecond: 0)!
+        let calendar = Calendar(date: date)
+
+        let from = cal.dateWithEra(1, year: 1981, month: 9, day: 10, hour: 21, minute: 0, second: 0, nanosecond: 0)!
+
+        XCTAssertEqual(37, calendar.dateIntervalFromDate(from))
+    }
+
     func testDateString() {
         let cal = NSCalendar(identifier: NSGregorianCalendar)!
         let date = cal.dateWithEra(1, year: 1981, month: 8, day: 4, hour: 20, minute: 0, second: 0, nanosecond: 0)!
         let calendar = Calendar(date: date)
-        println(calendar.dateString())
         XCTAssertEqual("8/4/81", calendar.dateString())
     }
 }
