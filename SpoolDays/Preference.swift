ef3ead3e9ff0860545a20ccd1a1e6d007871940f
Ryo SUETSUGU <suetsugu.r@gmail.com>
2015-04-22 23:42:24 +0900

    refactor

--- SpoolDays/Preference.swift
+++ SpoolDays/Preference.swift
@@ -3,13 +3,18 @@ import Foundation
 class Preference {
     var mintAPIKey: String {
         get {
-            let bundle = NSBundle.mainBundle()
-            if let path = bundle.pathForResource("preference", ofType: "plist") {
+            if let path = preferencePlistPath {
                 if let dictionary = NSDictionary(contentsOfFile: path) {
                     return dictionary.objectForKey("MintAPIKey") as? String ?? ""
                 }
             }
             return ""
+        }
+    }
+
+    var preferencePlistPath: String? {
+        get {
+            return NSBundle.mainBundle().pathForResource("preference", ofType: "plist")
         }
     }
 }
