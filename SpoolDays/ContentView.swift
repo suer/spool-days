00f2f14eaef44ddc9825ddc2b5345eb3145045f1
Ryo SUETSUGU <suetsugu.r@gmail.com>
2020-12-06 18:01:53 +0900

    Added: reset action

--- SpoolDays/ContentView.swift
+++ SpoolDays/ContentView.swift
@@ -5,6 +5,7 @@ struct ContentView: View {
 
     @State var showingActionSheet = false
     @State var showingEditView = false
+    @State var showingResetAlert = false
 
     @State var selectedDate: BaseDate? = nil
 
@@ -23,6 +24,15 @@ struct ContentView: View {
                         }) {
                             self.selectedDate.map { EditDateView(baseDate: $0) }
                         }
+                        .alert(isPresented: self.$showingResetAlert) {
+                            Alert(title: Text(I18n.confirmation),
+                                  message: Text(I18n.translate("Are you sure you want to reset date?")),
+                                  primaryButton: .default(Text(I18n.yes)) {
+                                    self.selectedDate.map { $0.reset(Date()) }
+                                  },
+                                  secondaryButton: .cancel(Text(I18n.no))
+                            )
+                        }
                         HStack {
                             Text(date.title)
                             Spacer()
@@ -39,7 +49,7 @@ struct ContentView: View {
                                     self.showingEditView = true
                                 },
                                 .default(Text(I18n.reset)) {
-
+                                    self.showingResetAlert = true
                                 },
                                 .default(Text(I18n.reset_with_date)) {
 
