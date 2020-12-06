0d3ff02233d5a6dad724fcbdcc36be0b4581db20
Ryo SUETSUGU <suetsugu.r@gmail.com>
2020-12-06 16:37:01 +0900

    Added: action sheet

--- SpoolDays/ContentView.swift
+++ SpoolDays/ContentView.swift
@@ -3,18 +3,42 @@ import SwiftUI
 struct ContentView: View {
     let datesViewModel = DatesViewModel.initWithFetched()
 
+    @State var showingActionSheet = false
+
     var body: some View {
         NavigationView {
             List {
                 // TODO: implement BaseDate as Identifiable
                 ForEach(self.datesViewModel.dates, id: \.sort) { date in
-                    HStack {
-                        Text(date.title)
-                        Spacer()
-                        Text("\(date.dateInterval()) \(I18n.translate("Days"))")
-                            .foregroundColor(Color.gray)
+                    ZStack {
+                        Button("") { self.showingActionSheet = true }
+                        HStack {
+                            Text(date.title)
+                            Spacer()
+                            Text("\(date.dateInterval()) \(I18n.translate("Days"))")
+                                .foregroundColor(Color.gray)
+                        }
                     }
                 }
+            }
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(title: Text("Action"),
+                            buttons: [
+                                .default(Text(I18n.edit)) {
+
+                                },
+                                .default(Text(I18n.reset)) {
+
+                                },
+                                .default(Text(I18n.reset_with_date)) {
+
+                                },
+                                .default(Text(I18n.history)) {
+
+                                },
+                                .cancel(Text(I18n.cancel))
+                            ]
+                )
             }
             .listStyle(GroupedListStyle())
             .navigationBarTitleDisplayMode(.inline)
