913d530f2c92c32522cae742bc57f551d601e0bd
Ryo SUETSUGU <suetsugu.r@gmail.com>
2025-06-28 12:48:44 +0900

    specify sqlite3 file

--- SpoolDays/CoreDataManager.swift
+++ SpoolDays/CoreDataManager.swift
@@ -5,9 +5,26 @@ class CoreDataManager {
     static let shared = CoreDataManager()
 
     private init() {}
+    lazy var applicationDocumentsDirectory: URL = {
+        let urls = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)
+        let applicationSupportURL = urls[urls.count-1]
+        // Get application name from bundle (same as MagicalRecord)
+        let applicationName = Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String ?? "SpoolDays"
+        let appDirectoryURL = applicationSupportURL.appendingPathComponent(applicationName)
+        // Create application directory if it doesn't exist
+        if !FileManager.default.fileExists(atPath: appDirectoryURL.path) {
+            try? FileManager.default.createDirectory(at: appDirectoryURL, withIntermediateDirectories: true, attributes: nil)
+        }
+        return appDirectoryURL
+    }()
 
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Model")
+        // Configure the persistent store to use spooldays.sqlite3
+        let storeURL = self.applicationDocumentsDirectory.appendingPathComponent("spooldays.sqlite3")
+        let description = NSPersistentStoreDescription(url: storeURL)
+        description.type = NSSQLiteStoreType
+        container.persistentStoreDescriptions = [description]
         container.loadPersistentStores { _, error in
             if let error = error {
                 fatalError("Core Data error: \(error)")
