ffac54ab22f66a8f29ac4b4603fbf8840dd48b94
mallowlabs <mallowlabs@gmail.com>
2026-05-30 20:35:40 +0900

    Add SpoolDaysDependencies/Package.swift

---
+++ SpoolDaysDependencies/Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.1.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SpoolDaysDependencies",
+    platforms: [
+        .iOS(.v18)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SpoolDaysDependencies",
+            targets: ["SpoolDaysDependencies"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/airbnb/HorizonCalendar.git", exact: "2.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SpoolDaysDependencies",
+            dependencies: [
+                "HorizonCalendar"
+            ]
+        )
+    ]
+)
